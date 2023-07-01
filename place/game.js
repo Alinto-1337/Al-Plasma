87f0ecad549b28b54d692db2ea188594ba569e4c
Alinto-1337 <AntonLisborg@gmail.com>
2023-07-01 22:58:24 +0200

    Working

--- place/game.js
+++ place/game.js
@@ -9,11 +9,17 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { x, y, color } = req.body;
+  console.log(req.body);
+  const dataArray = req.body;
   const mapData = fs.readFileSync('./place/storage/game.json', 'utf8');
-  newMapData = JSON.parse(mapData); // make to js array
-  newMapData[x + ((y-1)*20)] = color;
-  outData = JSON.stringify(newMapData);
+  const newMapData = JSON.parse(mapData); // make to js array
+  //console.log(newMapData[x + ((y-1)*20)])
+  dataArray.forEach(({ x, y, color }) => {
+    newMapData[x - 1 + ((y - 1) * 20)] = color; // Update the specific coordinates in the map data
+  });
+  //console.log(newMapData[(x + ((y-1)*20))])
+  const outData = JSON.stringify(newMapData);
+  console.log(outData);
   fs.writeFileSync('./place/storage/game.json', outData);
   res.send("write sucess");
 });
